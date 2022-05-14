133f6e4c5d99b2bb3adf9d26b21c676d5dded9e8
leekyuyoungweb <105539395+leekyuyoungweb@users.noreply.github.com>
2022-05-14 17:47:46 +0900

    Add files via upload

---
+++ 20220514/Test01.java
@@ -0,0 +1,28 @@
+
+public class Test01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// 변수 선언 및 할당
+		// 1. 정수형(int) 변수를 선언하고 그리고 적당한 값으로 할당
+		// 타입 변수명 = 값;
+		int number = 100;
+		System.out.println(number);
+		number = 125;
+		System.out.println(number);
+		
+		int number2 = 150;
+		
+		int number3 = number+number2;
+		
+		//변수 5개를 in line 에 표시하기 타입이 같을때
+//		int n1=0,n2=0,n3=0,n4=0,n5=0;
+		int n1,n2,n3,n4,n5;
+		n1=n2=n3=n4=n5=0;		
+		System.out.println(n1);
+		//변수는 사용하기전에 적당한 값으로 할당되어야 한다.
+		// 최초로변수에 값을 할당하는 행위를 초기화라고 함
+		// 초기화 방법엔은 변수선언과 동시에  / 선언후에 하는 방법
+	}
+
+}
