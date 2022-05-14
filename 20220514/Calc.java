4b7cce27a654de551cae08bbc53383c7e584f9ab
leekyuyoungweb <105539395+leekyuyoungweb@users.noreply.github.com>
2022-05-14 17:53:18 +0900

    Add files via upload

---
+++ 20220514/Calc.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ * @author 이규영
+ *	수업마지막 과제...
+ * 내용 : 두개의 변수를 이용한 사칙연산
+ */
+public class Calc {
+
+	public static void main(String[] args) {
+		int number1 = 100, number2 = 2;
+		System.out.println("+ : " + (number1+number2));
+		System.out.println("- : " + (number1-number2));
+		System.out.println("x :" + (number1*number2));
+		System.out.println("/ :" + (number1/number2));
+	}
+
+}
